dc07f45e91108264b6695111007854fce79eeee2
ashwinm-oo7 <ashwinmaurya9211@gmail.com>
2025-03-16 18:13:00 +0530

    updates succ

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "./",
+    clean: true,
+  },
+  mode: "production",
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Process .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"], // Ensure Webpack resolves .js and .jsx
+  },
+};
